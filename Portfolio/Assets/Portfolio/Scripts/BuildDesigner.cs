a4642c56b1394cd2424282715a7247334fc682a6
AsrielDreemurr0083 <103588221+AsrielDreemurr0083@users.noreply.github.com>
2024-09-03 15:46:39 +0900

    포트폴리오

---
+++ Portfolio/Assets/Portfolio/Scripts/BuildDesigner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildDesigner : MonoBehaviour
+{
+    public GameObject buildingPrefab;
+    public Vector3[] spawnPositions;
+
+    void Start()
+    {
+        
+    }
+
+    void SpawnBuilding()
+    {
+        foreach(Vector3 position in spawnPositions)
+        {
+
+        }
+    }
+}
